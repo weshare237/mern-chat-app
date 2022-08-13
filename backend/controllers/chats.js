3195cd5e3ba4436b2b67ab0c7d70306ace88ef4b
weshare237 <fopaduclair2000@gmail.com>
2022-08-13 21:00:46 +0100

    Group rename, group add user and group remove user

--- backend/controllers/chats.js
+++ backend/controllers/chats.js
@@ -1,7 +1,7 @@
 const Chat = require('../models/Chat')
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
+const { BadRequestError, NotFoundError } = require('../errors')
 
 const createChat = async (req, res) => {
   const { userId } = req.body
@@ -96,11 +96,90 @@ const getAllChats = async (req, res) => {
   res.status(StatusCodes.OK).json(chats)
 }
 
-const renameGroup = async (req, res) => {}
+const renameGroup = async (req, res) => {
+  const { name } = req.body
 
-const removeFromGroup = async (req, res) => {}
+  if (!name) {
+    throw new BadRequestError('Group name not provided')
+  }
 
-const addToGroup = async (req, res) => {}
+  const group = await Chat.findOneAndUpdate(
+    { _id: req.params.id },
+    { chatName: name },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password')
+
+  if (!group) {
+    throw new NotFoundError(`No chat group found with id ${req.params.id}`)
+  }
+
+  res.status(StatusCodes.OK).json(group)
+}
+
+const removeFromGroup = async (req, res) => {
+  const { userId } = req.body
+  const { id: groupId } = req.params
+
+  console.log(userId, groupId)
+
+  if (!userId) {
+    throw new BadRequestError('User not provided')
+  }
+
+  const group = await Chat.findByIdAndUpdate(
+    groupId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password')
+
+  console.log(group)
+
+  if (!group) {
+    throw new NotFoundError(`No chat group found with id ${groupId}`)
+  }
+
+  res.status(StatusCodes.OK).json(group)
+}
+
+const addToGroup = async (req, res) => {
+  const { userId } = req.body
+  const { id: groupId } = req.params
+
+  if (!userId) {
+    throw new BadRequestError('User not provided')
+  }
+
+  const group = await Chat.findByIdAndUpdate(
+    groupId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password')
+
+  if (!group) {
+    throw new NotFoundError(`No chat group found with id ${groupId}`)
+  }
+
+  res.status(StatusCodes.OK).json(group)
+}
 
 module.exports = {
   getAllChats,
